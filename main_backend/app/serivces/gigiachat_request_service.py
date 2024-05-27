80a0382eee8b33d86bcf22707df6758f8ce4462d
losefaleksei <48041279+losefaleksei@users.noreply.github.com>
2024-05-27 20:03:36 +0700

    Update gigiachat_request_service.py
    вытащил отправку промта из цикла for ent in doc.ents

--- main_backend/app/serivces/gigiachat_request_service.py
+++ main_backend/app/serivces/gigiachat_request_service.py
@@ -30,32 +30,29 @@ def api_text_generation(analyse_text: str, reference_text):
     for ent in doc.ents:
         if ent.label_ in ['PERSON', 'DATE']:
             cleaned_text = cleaned_text.replace(ent.analyse_text, '')  
-        access_token = api_auth()
-        text_generation_url = "https://gigachat.devices.sberbank.ru/api/v1/chat/completions"
-
-        text_ganaration_payload = json.dumps({
-        "model": "GigaChat:latest",
-        "messages": [
-            {
-            "role": "user",
-            "content": f'От лица специалиста, расскажи о результатах анализа из первого текста - {cleaned_text} исходя из информации о значениях во втором - {reference_text}. расшифруй результаты таким образом, чтобы любой человек понял. В своём ответе не указывай, что ты опирался на какой-либо текст, а просто дай своё заключение'
-            }
-        ],
-        "temperature": 0.5,
-        "top_p": 0.1,
-        "n": 1,
-        "stream": False,
-        "max_tokens": 1024,
-        "repetition_penalty": 1
-        })
-
-        text_generation_headers = {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-        'Authorization': f'Bearer {access_token}'
+            
+    access_token = api_auth()
+    text_generation_url = "https://gigachat.devices.sberbank.ru/api/v1/chat/completions"
+    text_ganaration_payload = json.dumps({
+    "model": "GigaChat:latest",
+    "messages": [
+        {
+        "role": "user",
+        "content": f'От лица специалиста, расскажи о результатах анализа из первого текста - {cleaned_text} исходя из информации о значениях во втором - {reference_text}. расшифруй результаты таким образом, чтобы любой человек понял. В своём ответе не указывай, что ты опирался на какой-либо текст, а просто дай своё заключение'
         }
-
-        text_generation_response = requests.request("POST", text_generation_url, headers=text_generation_headers, data=text_ganaration_payload, verify=False)
-
-        print(text_generation_response.json()['choices'])
-        return text_generation_response.json()['choices'][0]['message']['content']
+    ],
+    "temperature": 0.5,
+    "top_p": 0.1,
+    "n": 1,
+    "stream": False,
+    "max_tokens": 1024,
+    "repetition_penalty": 1
+    })
+    text_generation_headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'Authorization': f'Bearer {access_token}'
+    }
+    text_generation_response = requests.request("POST", text_generation_url, headers=text_generation_headers, data=text_ganaration_payload, verify=False)
+    print(text_generation_response.json()['choices'])
+    return text_generation_response.json()['choices'][0]['message']['content']
